d6976075eeafe6557c5881f8b8bb76c1c90b04de
JIdigit <emirbek.b@gmail.com>
2019-11-16 15:56:25 +0600

    p2 task 14

---
+++ task14.py
@@ -0,0 +1,10 @@
+numbers=[1,2,3,4,5,6,7,8,9]
+i=0
+if numbers[i]%2==0:
+    for numbers[i] in [2,4,6,8]:
+        print(numbers[i], end='-')
+        i+=1
+else:
+    for numbers[i] in [2,4,6,8]:
+        print(numbers[i], end='-')
+        i+=1
